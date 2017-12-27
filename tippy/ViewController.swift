c66c66a214e917a734581f55653f11ea905dca7c
Aung Thu Phyo <stevewayne@SteveWaynes-MacBook-Pro.local>
2017-12-27 23:27:42 +0900

    Swift4
    Simple Tip-Calculator

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var textFiledLabel: UITextField!
+    @IBOutlet weak var Control: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func textFieldCalculate(_ sender: UITextField) {
+        calculate()
+    }
+    
+    @IBAction func valueChanged(_ sender: UISegmentedControl) {
+        calculate()
+    }
+    
+    func calculate(){
+        let segmentedConrtrol = [0.18,0.2,0.25]
+        
+        let bill = Double(textFiledLabel.text!) ?? 0
+        let tip = bill * segmentedConrtrol[Control.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
